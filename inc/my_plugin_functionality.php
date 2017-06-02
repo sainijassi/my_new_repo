ba1f945df443045876c6e0bb7b1c35038f18e589
sainijassi <jasvirsaini03@gmail.com>
2017-06-02 11:03:39 +0530

    first commit

---
+++ inc/my_plugin_functionality.php
@@ -0,0 +1,35 @@
+<doctype html>
+<html>
+	<head>
+		 <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+	
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-5">
+			<p class="bg-info">For adding the single line text container click below</p> <br>
+			<button class="btn btn-info single_line">
+				Add single line text
+			</button>
+
+
+		</div>
+	
+
+	<div class="col-xs-5">
+			<p>For adding the password click below </p><br>
+			<button class="btn btn-info password">
+				enter you password
+			</button>
+
+
+		</div>
+
+		</div>
+	</body>
+</html>
